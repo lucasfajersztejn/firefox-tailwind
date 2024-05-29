b386482f7ac6145256e79526ca9ff7b8f26cdcd1
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-05-29 15:20:13 +0200

    Hero created, now working on it

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Hero from './components/ui/Hero/Hero'
 import Navbar from './components/ui/Navbar/Navbar'
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Hero />
       <h1 className='text-3xl'>Hola</h1>
     </>
   )
---
+++ src/components/ui/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+
+function Hero() {
+  return (
+    <div className="container mx-auto flex px-5 md:py-24 md:flex-row flex-col items-center relative pt-40">
+      
+    </div>
+  )
+}
+
+export default Hero;
