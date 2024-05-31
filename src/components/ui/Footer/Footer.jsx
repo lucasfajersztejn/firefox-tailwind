5aa77395e72f97f01488c36b3dbcdc8d51b3f31b
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-05-31 11:47:32 +0200

    Improvements in the footer

--- src/components/ui/Footer/Footer.jsx
+++ src/components/ui/Footer/Footer.jsx
@@ -1,38 +1,39 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import firefoxImg from "../../../assets/img/firefox.png";
 
 function Footer() {
   return (
     <footer className="bg-white dark:bg-gray-900">
       <div className="container p-6 mx-auto">
-        <div className="lg:flex">
-          <div className="w-full -mx-6 lg:w-2/5">
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-10">
+          <div className="w-full -mx-6 lg:w-2/5 flex flex-col lg:flex-row justify-center items-center">
             <div className="px-6">
               <a href="https://www.mozilla.org/en-US/firefox/new/">
-                <img className="w-auto h-7" src={firefoxImg} alt="Firefox logo" />
+                <img className="w-auto h-7 mx-auto lg:mx-0 hover:animate-bounce" src={firefoxImg} alt="Firefox logo" />
               </a>
 
-              <p className="max-w-sm mt-2 text-gray-500 dark:text-gray-400">Visit Mozilla Corporation’s not-for-profit parent, the Mozilla Foundation. Portions of this content are ©1998–2024 by individual mozilla.org contributors. Content available under a Creative Commons license.</p>
+              <p className="max-w-sm mt-2 text-gray-500 dark:text-gray-400 text-center lg:text-left">Visit Mozilla Corporation’s not-for-profit parent, the Mozilla Foundation. Portions of this content are ©1998–2024 by individual mozilla.org contributors. Content available under a Creative Commons license.</p>
 
               <div className="Container mx-auto px-5 py-8 flex items-center">
-                <div className="w-full flex justify-start gap-10">
+                <div className="w-full flex justify-center lg:justify-start gap-10">
                   <a href="https://www.linkedin.com/in/lucas-fajersztejn/" className="text-white cursor-pointer">
-                    <i className="fa-brands fa-facebook-f text-2xl"></i>
+                    <i className="fa-brands fa-facebook-f text-2xl text-black dark:text-white hover:animate-bounce"></i>
                   </a>
 
                   <a href="https://www.linkedin.com/in/lucas-fajersztejn/" className="text-white cursor-pointer">
-                    <i className="fa-brands fa-x-twitter text-2xl"></i>
+                    <i className="fa-brands fa-x-twitter text-2xl text-black dark:text-white hover:animate-bounce"></i>
                   </a>
 
                   <a href="https://www.linkedin.com/in/lucas-fajersztejn/" className="text-white cursor-pointer">
-                    <i className="fa-brands fa-instagram text-2xl"></i>
+                    <i className="fa-brands fa-instagram text-2xl text-black dark:text-white hover:animate-bounce"></i>
+                  </a>
+
+                  <a href="https://github.com/lucasfajersztejn/firefox-tailwind" className="text-white cursor-pointer">
+                    <i className="fa-brands fa-github text-2xl text-black dark:text-white hover:animate-bounce"></i>
                   </a>
 
                   <a href="https://www.linkedin.com/in/lucas-fajersztejn/" className="text-white cursor-pointer">
-                    <i className="fa-brands fa-github text-2xl"></i>
-                  </a>
-
-                  <a href="https://www.linkedin.com/in/lucas-fajersztejn/" className="text-white cursor-pointer">
-                    <i className="fa-brands fa-linkedin-in text-2xl"></i>
+                    <i className="fa-brands fa-linkedin-in text-2xl text-black dark:text-white hover:animate-bounce"></i>
                   </a>
                 </div>
 
@@ -41,7 +42,7 @@ function Footer() {
           </div>
 
           <div className="mt-6 lg:mt-0 lg:flex-1">
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="grid grid-cols-2 gap-6 md:grid-cols-4">
               <div>
                 <h3 className="text-gray-700 uppercase dark:text-white">About</h3>
                 <a href="#" className="block mt-2 text-sm text-gray-600 dark:text-gray-400 hover:underline">Company</a>
@@ -84,27 +85,3 @@ function Footer() {
 
 export default Footer;
 
-/*
-<footer className="bg-gradient-to-r from-indigo-700 via-purple-700 to-pink-700 ">
-      <div className="Container mx-auto px-5 py-8 flex items-center">
-        <div className="w-full flex justify-center gap-10">
-          <a href="" className="text-white cursor-pointer">
-            <i className="fa-brands fa-facebook-f text-2xl"></i>
-          </a>
-
-          <a href="" className="text-white cursor-pointer">
-            <i className="fa-brands fa-x-twitter text-2xl"></i>
-          </a>
-
-          <a href="" className="text-white cursor-pointer">
-            <i className="fa-brands fa-instagram text-2xl"></i>
-          </a>
-
-          <a href="" className="text-white cursor-pointer">
-            <i className="fa-brands fa-linkedin-in text-2xl"></i>
-          </a>
-        </div>
-
-      </div>
-    </footer>
-*/
