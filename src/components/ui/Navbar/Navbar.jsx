6a0c372589d57ec87d7c994bc3671307ad844e7d
lucasfajersztejn <96478667+lucasfajersztejn@users.noreply.github.com>
2024-06-01 18:14:09 +0200

    Improvements in the dark mode button

--- src/components/ui/Navbar/Navbar.jsx
+++ src/components/ui/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar({ onDarkMode, darkMode }) {
     <nav className="z-20 bg-white dark:bg-slate-800 fixed md:static w-full md:w-auto border-b md:border-none shadow-lg md:shadow-none dark:border-none">
       <div className="p-6 container mx-auto">
         <div className="flex items-center justify-between">
-          <div className="z-20">
+          <div className="z-20 mx-auto lg:mx-0">
             <a href="" className="flex items-center">
               <img src={firefoxImage} alt="Logo image" className="w-14 h-14"/>
-              <span className="text-3xl ml-3"> <span className="font-bold">Firefox</span> Browser </span>
+              <span className="text-3xl ml-3 md:hidden lg:block"> <span className="font-bold">Firefox</span> Browser </span>
             </a>
           </div>
 
@@ -29,10 +29,19 @@ function Navbar({ onDarkMode, darkMode }) {
                 <a href="" className="tracking-wide order-4 md:order-none cursor-pointer px-3 py-2 rounded font-semibold text-lg transition hover:text-white hover:bg-pink-500">Lenguages</a>
 
                 <label className="flex cursor-pointer gap-2">
-                  <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-                  <input onClick={onDarkMode} type="checkbox" value="synthwave" className="toggle theme-controller"/>
-                  <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
-                  
+                  {window.matchMedia("(prefers-color-scheme: dark)").matches ? (
+                    <>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                      <input onClick={onDarkMode} type="checkbox" value="synthwave" className="toggle theme-controller"/>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>  
+                    </>
+                  ) : (
+                    <>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
+                      <input onClick={onDarkMode} type="checkbox" value="synthwave" className="toggle theme-controller"/>
+                      <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
+                    </>
+                  )}
                 </label>
               
               </div>
